32102706239d62f2c3d37fb522359b6aafbb15a1
Deka-Debajit <debajit2deka@gmail.com>
2024-08-05 10:39:49 +0530

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Bootstrap Navbar</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom CSS -->
+  <style>
+    .custom-navbar {
+      background-color: #4CAF50; /* Change the color here */
+      height: 80px; /* Increase the height here */
+    }
+    .custom-navbar .navbar-brand,
+    .custom-navbar .nav-link {
+      color: #ffffff !important; /* Change the link color here */
+    }
+    .custom-navbar .navbar-toggler-icon {
+      filter: invert(1); /* Make the toggler icon white */
+    }
+    .custom-navbar .form-control {
+      height: calc(1.5em + .75rem + 2px); /* Adjust input height */
+    }
+    .custom-navbar .btn {
+      height: calc(1.5em + .75rem + 2px); /* Adjust button height */
+    }
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg custom-navbar">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">E-commerce</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Orders</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Add To Cart</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<!-- Bootstrap JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
